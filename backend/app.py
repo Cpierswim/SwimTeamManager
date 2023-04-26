29a66f871d8e0549f2e21c27390e919fc1ad622f
Chris Pierson <cpierswim@gmail.com>
2023-04-26 14:31:54 -0400

    NEW: Teams part of API working inc. secret key to add a team

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from database.models import db
 from database.schemas import ma
 from resources.auth import LoginResource, RegisterResource
 #from resources.cars import AllCarResource, UserCarResource
+from resources.team import TeamResource, TeamsResource
 from dotenv import load_dotenv
 from os import environ
 
@@ -56,5 +57,7 @@ def create_routes():
     # api.add_resource(AllCarResource, '/api/cars')
     # api.add_resource(UserCarResource, '/api/user_cars')
     # TODO: Create files for your Resources in resources folder, add them here
+    api.add_resource(TeamResource, '/api/team/<int:team_id>')
+    api.add_resource(TeamsResource, '/api/teams/')
     
     return api
--- backend/resources/team.py
+++ backend/resources/team.py
@@ -3,8 +3,31 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_req
 from flask_restful import Resource
 from database.models import db, Team
 from database.schemas import team_schema
+from dotenv import load_dotenv
+from os import environ
+
+load_dotenv()
+secret_key = environ.get('ADD_TEAM_SECRET_KEY')
 
 class TeamResource(Resource):
     def get(self, team_id):
         team = Team.query.get_or_404(team_id)
         return team_schema.dump(team), 200
+    
+class TeamsResource(Resource):  
+    def post(self):
+        #if(form_data.secret_key == secret_key):
+        #    stop = 0
+        form_data = request.get_json()
+        try:
+            if(form_data["secret_key"] != secret_key):
+                return "Cannot add new Teams", 405
+            else:
+                del form_data["secret_key"]
+                new_team = team_schema.load(form_data)
+                db.session.add(new_team)
+                db.session.commit()
+                return team_schema.dump(new_team), 201
+        except:
+            return "Cannot Add New Teams", 403
+        
