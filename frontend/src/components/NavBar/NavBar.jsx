0c919a0aaf6471924972920eea6fbe527a2c542b
Chris Pierson <cpierswim@gmail.com>
2023-04-27 20:32:49 -0400

    NEW: Nav bar setup

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,28 +2,62 @@ import React from "react";
 import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import "./NavBar.css";
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Flask JWT</b>
-          </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample08"
+        aria-controls="navbarsExample08"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        className="collapse navbar-collapse justify-content-md-center"
+        id="navbarsExample08"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Swim Team Manager
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Parent
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Coach
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
